d01f8edc7c4fcebe49d350c1cd18fdb3e01b9a54
anascode23 <anas.ali@ecutbildning.se>
2024-01-24 14:10:00 +0000

    Add project files.

---
+++ FactsWebApp/Data/Migrations/20240123134110_first migration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FactsWebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FactQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FactAnswer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fact", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fact");
+        }
+    }
+}
